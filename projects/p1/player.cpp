45bfe46398e50b1f60d46931c630246d984408f0
pobeng <b24peng@uwaterloo.ca>
2016-06-19 02:25:31 -0400

    initial version of legal plays

--- projects/p1/player.cpp
+++ projects/p1/player.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 Player::Player() {}
 
+/* Get functions */
 vector< shared_ptr<Card> > Player::getOriginalCards() const {
     return originalCards_;
 }
@@ -20,6 +21,32 @@ vector< shared_ptr<Card> > Player::getDiscardedCards() const {
 int Player::getScore() const {
     return score_;
 }
+
+// // Get legal plays
+// vector< shared_ptr<Card> > Player::getLegalPlays() {
+//   //loop through player's current cards
+//   bool addLegalPlay = true;
+//   vector< shared_ptr<Card> > legalPlays;
+//   // Outer loop: current cards in hand
+//   for (int i = 0; i < Player::getCurrentCards().size(); i++){
+//     addLegalPlay = true;
+//     for (int j = 0; j < totalPlayedCards_.size(); j++){
+//       // If card in hand has same suit and is +/- 1 rank of a played card, then it is legal. It is also legal if card is a 7
+//       if((*totalPlayedCards_.at(j).getSuit() == (*getCurrentCards().at(i)).getSuit() && ( *totalPlayedCards_.at(j).getRank() == (*getCurrentCards().at(i)).getRank() + 1 ||
+//       *totalPlayedCards_.at(j).getRank() == (*getCurrentCards().at(i)).getRank() - 1)) || (*getCurrentCards().at(i)).getRank() == 7){
+//         if(addLegalPlay){
+//           legalPlays.push_back(getCurrentCards().at(i));
+//           addLegalPlay = false;
+//         }
+//       }
+//     }
+//   }
+//   return legalPlays;
+// }
+
+
+
+
 
 void Player::addOriginalCard(shared_ptr<Card> card) {
     originalCards_.push_back(card);
