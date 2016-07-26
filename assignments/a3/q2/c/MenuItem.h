506a1d0318e32a5d58c3640c74677dfe6023548c
Gautam Gupta <gautam@gaut.am>
2016-07-26 18:36:34 -0400

    q2c (use provided code)

---
+++ assignments/a3/q2/c/MenuItem.h
@@ -0,0 +1,42 @@
+#ifndef MENU_ITEM_H
+#define MENU_ITEM_H
+
+#include <iostream>
+#include <string>
+
+#include "ComponentIterator.h"
+#include "MenuComponent.h"
+
+class MenuItem : public MenuComponent {
+public:
+    class iterator;
+    friend class iterator;
+    class iterator : public ComponentIterator {
+    public:
+        iterator( MenuItem* mi = 0 );
+    };
+
+    // constructor
+    MenuItem(const std::string, float, int, bool);
+    virtual ~MenuItem() {}
+
+    // implement leaf operations
+    virtual bool isVeggie() const;
+    virtual float price() const;
+    virtual void priceIs( float p );
+    virtual int calories() const;
+    virtual void caloriesIs( int c );
+
+    // implement shared operations
+    virtual ComponentIterator begin();
+    virtual ComponentIterator end();
+
+protected:
+    virtual void print ( std::ostream& ) const;
+private:
+    float price_;
+    int calories_;
+    bool isVeggie_;
+};
+
+#endif
